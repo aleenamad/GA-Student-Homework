9198385764c82794f9c385ca178889980b8e6752
AleenaMad <aleena.madni@gmail.com>
2017-12-06 21:35:28 -0600

    commit 3 - Excited kittens answered

--- answers.js
+++ answers.js
@@ -10,3 +10,14 @@ for (let i = 0; i <= 200; i++) {
     console.log(i);
   }
 }
+// Exited Kitten
+// 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+for (let i = 0; i <= 20; i++) {
+  console.log('Love me, pet me! HSSSSSSS!');
+}
+// 2. For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+for (let i = 0; i <= 20; i++) {
+  if (i %2===0) {
+  console.log("...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away...");
+  }
+}
