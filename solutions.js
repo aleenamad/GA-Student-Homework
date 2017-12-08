3b61b81c5713d9bd50f022507ef454a569b7eca0
AleenaMad <aleena.madni@gmail.com>
2017-12-08 13:47:01 -0600

    add check prime

--- solutions.js
+++ solutions.js
@@ -5,22 +5,42 @@
 // We specify the output of a function with the return statement. The return statement is different from console.log() in that we can use the return value of a function to pass as data, whereas we cannot with a console.log().
 
 //Palindrome
-const checkPalindrome = (word) => {
-  var re = /[\W_]/g;
-  var lowRegWord = word.toLowerCase().replace(re, '');
-  var reverseWord = lowRegWord.split('').reverse().join('');
-  return reverseWord === lowRegWord;
-}
-
-console.log(checkPalindrome("radar"));
-console.log(checkPalindrome("Borscht"));
+// const checkPalindrome = (word) => {
+//   var re = /[\W_]/g;
+//   var lowRegWord = word.toLowerCase().replace(re, '');
+//   var reverseWord = lowRegWord.split('').reverse().join('');
+//   return reverseWord === lowRegWord;
+// }
+//
+// console.log(checkPalindrome("radar"));
+// console.log(checkPalindrome("Borscht"));
 
 // Digit Sum
 // const sumDigits = (num) => {
-//   if (num = )
+//   let sum=0;
+//   const numToString = num.toString();
+//   for (let i =0; i < numToString.length; i++) {
+//     sum+=parseInt(numToString[i]);
+//
+//   }
+//   return sum
+// }
+//
+// console.log(sumDigits(42));
+
+// const calculateSide = (sideA, sideB) => {
+//
 // }
 
+// check prime
+const checkPrime = (num) => {
+if (num%2 !=0) {
+  return true }
+if (num%2 == 0) {
+  return false }
 
-const calculateSide = (sideA, sideB) => {
-  
 }
+console.log(checkPrime(9));
+console.log(checkPrime(10));
+console.log(checkPrime(5));
+console.log(checkPrime(7));
