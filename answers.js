66a3db0e098ba3a5f0e021fae880cac7e69642d9
AleenaMad <aleena.madni@gmail.com>
2017-12-07 22:03:53 -0600

    Commit 1 - verbal questions

--- answers.js
+++ answers.js
@@ -1,3 +1,4 @@
+// homework for wednesay December 6th
 // Easy Going
 // 1. Write a for loop that will log the numbers 1 through 20.
 for (let i = 1; i <= 20; i++) {
@@ -26,3 +27,50 @@ for (let i = 0; i <= 20; i++) {
 for (let i = 1; i <= 100; i++) {
   console.log(i);
 for (let i % 3; )
+
+
+
+// Getting to know you
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+
+
+// Return of the closets
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+// Kristyn's left shoe has traveled through time and space and turned up in Thom's accessories drawer! Remove Kristyn's shoe from the array and save it to the variable kristynsShoe. Use that variable to add Kristyn's lost shoe to Thom's accessories array.
+
+let kristynsShoe = kristynsCloset[0];
+thomsCloset[2].push(kristynsShoe);
---
+++ solutions.js
@@ -0,0 +1,5 @@
+// Functions
+// 1. What is the difference between a parameter and an argument?
+// - The argument is the input, the parameter is how the input is represented in the function.
+// 2. Within a function, what is the difference between return and console.log?
+// We specify the output of a function with the return statement. The return statement is different from console.log() in that we can use the return value of a function to pass as data, whereas we cannot with a console.log().
