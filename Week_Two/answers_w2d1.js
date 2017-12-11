a7d37adf16ecfec5b97695609cab8714e1c9b15e
AleenaMad <aleena.madni@gmail.com>
2017-12-11 17:06:38 -0600

    1st commit-selecting datatypes

---
+++ Week_Two/answers_w2d1.js
@@ -0,0 +1,31 @@
+/*
+1. A light switch that can be either on or off.
+-
+2. A user's email address.
+- object:
+- const user {
+name:
+phone number:
+email address:
+}
+-could also be with arrays
+3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+-
+4. A list of student names from our class.
+-arrays or objects
+const studentNames = ["name", "name", "name"]
+const studentNames {
+name:
+name:
+name:
+}
+5. A list of student names from our class, each with a location.
+-arrays and objects
+const student{
+{ name:, location: }
+}
+6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+-arrays and objects
+const student {
+{Name:, location, tvShows: [""] },
+}
