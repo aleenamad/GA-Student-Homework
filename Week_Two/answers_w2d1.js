197d4b8eb55e57030839af1257e01d3887a8c255
AleenaMad <aleena.madni@gmail.com>
2017-12-11 23:28:53 -0600

    3rd commit- performed operations on the crazy object

--- Week_Two/answers_w2d1.js
+++ Week_Two/answers_w2d1.js
@@ -33,15 +33,53 @@ const student {
 */
 
 // Take it Easy
-const colorOfRainbow = ["red", "orange", "yellow","green", "blue", "indigo", "voilet"];
+// const colorOfRainbow = ["red", "orange", "yellow","green", "blue", "indigo", "voilet"];
+//
+// console.log(colorOfRainbow[4]);
+//
+// const aleena = {
+//   favoriteFood: "pizza",
+//   hobby: "collect rare sneakers",
+//   currentLocation: "Allen",
+//   favoriteDatatype: "objects",
+// }
+//
+// console.log(aleena.hobby);
+//
 
-console.log(colorOfRainbow[4]);
-
-const aleena = {
-  favoriteFood: "pizza",
-  hobby: "collect rare sneakers",
-  currentLocation: "Allen",
-  favoriteDatatype: "objects",
+// Crazy Object!
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
 }
-
-console.log(aleena.hobby);
+// 1
+console.log(crazyObject.taco[1].salsa[5]);
+// 2
+console.log(crazyObject.larry.quotes[0]);
+// 3
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+// 4
+console.log(crazyObject.larry.nicknames[1]);
+// 5
+console.log(crazyObject.larry.characters[1]);
