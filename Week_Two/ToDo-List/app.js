931da61a45a6fffc866461124eeaed26def694d6
AleenaMad <aleena.madni@gmail.com>
2017-12-15 14:41:43 -0600

    add todo list project

---
+++ Week_Two/ToDo-List/app.js
@@ -0,0 +1,51 @@
+let main = function () {
+  $('.btn').click(function() {
+    let post = $('.todo-box').val();
+    $('<li>').text(post).prependTo('.posts');
+    $('.todo-box').val('');
+    $('.counter').text('300');
+    $('.btn').addClass('disabled');
+  });
+
+  $('.todo-box').keyup(function() {
+    let postLength = $(this).val().length;
+    let charactersLeft = 300 - postLength;
+    $('.counter').text(charactersLeft);
+
+    if(charactersLeft < 0) {
+      $('.btn').addClass('disabled');
+    }
+    else if(charactersLeft == 300) {
+      $('.btn').addClass('disabled');
+    }
+    else {
+      $('.btn').removeClass('disabled');
+    }
+});
+
+$('.btn').addClass('disabled');
+
+
+}
+
+
+
+$(document).ready(function($){
+  $('form').submit(function() {
+    if ($('.todo-box').val() !== '') {
+      let newTask = $('.todo-box').val();
+      let newLi = $('<li>' + newTask + '</li>');
+      newLi.on('click', function() {
+        $(this).remove();
+      });
+      $('ol').prepend(newLi); // to put the new task on top
+      $('.todo-box').val('');
+      return false;
+    }
+  });
+  // $('ol').sortable();  
+});
+
+
+
+$(document).ready(main);
