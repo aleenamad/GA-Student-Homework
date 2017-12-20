35adb79dc88acb7580336b2b94be18c21f52e188
AleenaMad <aleena.madni@gmail.com>
2017-12-20 08:43:41 -0600

    add some more jquer

--- Week_3/Day_2/currency_converter/app.js
+++ Week_3/Day_2/currency_converter/app.js
@@ -1,7 +1,18 @@
 function convertCurrency(event) {
   if ('type' in event.target && event.target.type === 'button') {
     console.log('handle currency conversion here');
+    //$.get("https://api.fixer.io/latest?base=USD&symbols", function(response) {
+
+  // }
   }
 }
+
+
+
+const currencyPutIn =  Number($('#insertCurrency').val());
+const conversionRate = Number
+
+
+
 
 document.body.addEventListener('click', convertCurrency);
